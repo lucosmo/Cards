4e58d088fd5e23c5749c4bbf37195961c7f6eedb
lucosmo <lukasz.kosmaczewski@gmail.com>
2023-12-18 15:27:50 +0000

    updated base address path

--- MyCards.API/MyCards.Frontend/Client/Program.cs
+++ MyCards.API/MyCards.Frontend/Client/Program.cs
@@ -11,8 +11,7 @@ namespace MyCards.Frontend.Client
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
             builder.RootComponents.Add<HeadOutlet>("head::after");
-
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri($"{builder.HostEnvironment.BaseAddress.TrimEnd('/')}/bff/") });
 
             await builder.Build().RunAsync();
         }

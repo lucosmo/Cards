f032328c901a8f68773bd76b717ec8a7f0d07e73
lucosmo <lukasz.kosmaczewski@gmail.com>
2023-12-18 15:29:45 +0000

    created mapper for working with response, and creating CardData object from it

---
+++ MyCards.API/MyCards.Frontend/Server/Mapper/CardMapper.cs
@@ -0,0 +1,19 @@
+﻿using MyCards.Frontend.Shared.Data;
+
+namespace MyCards.Frontend.Server.Mapper
+{
+    public class CardMapper
+    {
+        public static CardData MapCard(int id, string title, string fileReference, DateTime createdAt, bool fileLinked)
+        {
+            return new CardData()
+            {
+                Id = id,
+                Title = title,
+                FileReference = fileReference,
+                CreatedAt = createdAt,
+                FileLinked = fileLinked
+            };
+        }
+    }
+}
